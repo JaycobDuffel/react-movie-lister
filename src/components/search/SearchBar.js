d45e9ad878eaa6491fba933e1d39517918642752
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:45:09 -0800

    Add SearchBar imports

---
+++ src/components/search/SearchBar.js
@@ -0,0 +1,6 @@
+import React, { useContext, forwardRef } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+
+import { FilterContext } from '../../context/Context';
+import { searchBarBackgroundColor, searchBarTextColor } from '../../constants/colors';
