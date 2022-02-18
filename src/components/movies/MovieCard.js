2c0ea549958d559f7a9420e3e52f0e934238b15d
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:41:13 -0800

    Import components and constants in MovieCard.js

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, View, Text } from 'react-native';
+
+import { cardBackgroundColor } from '../../constants/colors';
+import MovieButton from './MovieButton';
+
