ae9b7acb947bbea918f69f61904a60b051701053
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:43:12 -0800

    Add MovieList component

--- src/components/movies/MovieList.js
+++ src/components/movies/MovieList.js
@@ -4,3 +4,19 @@ import Constants from 'expo-constants';
 
 import { MovieContext } from '../../context/Context';
 import MovieCard from './MovieCard';
+
+export default function MovieList() {
+  const { movies } = useContext(MovieContext);
+  const renderItem = ({ item }) => (
+    <MovieCard movie={item} />
+  );
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={movies}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => `${item.imdbID}${index}`}
+        contentContainerStyle={styles.flatList} />
+    </View>
+  );
+}
