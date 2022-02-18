64e77a8ebf37c863801a05b0dec6613682be6253
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:42:04 -0800

    Create MovieCard Component

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -4,3 +4,21 @@ import { Image, StyleSheet, View, Text } from 'react-native';
 import { cardBackgroundColor } from '../../constants/colors';
 import MovieButton from './MovieButton';
 
+export default function MovieCard({ movie }) {
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={styles.poster}
+          source={movie.Poster === 'N/A' ? require('../../../assets/missing_poster.jpeg') : { uri: movie.Poster }}
+        />
+      </View>
+      <View style={styles.movieDetails}>
+        <Text>{movie.Title}</Text>
+        <Text>{movie.Year}</Text>
+        <MovieButton title="Details" />
+      </View>
+    </View>
+  );
+}
