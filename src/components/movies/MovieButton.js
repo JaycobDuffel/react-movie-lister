a15521e83373059ef20eace816c7c6e3ab8b3193
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:49:32 -0800

    Seperate package imports from created import

--- src/components/movies/MovieButton.js
+++ src/components/movies/MovieButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
 import { cardButtonBackgroundColor } from '../../constants/colors';
 
 export default function MovieButton({ title, style, onPress }) {
