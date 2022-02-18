dc06be9e0ba90ed1717b1f73838f44caad47d40e
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:31:37 -0800

    Add constants for colours

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+export const mainBackgroundColor = '#E1E6E6';
+export const searchBarBackgroundColor = '#FDCDBA';
+export const searchBarTextColor = '#000';
+export const cardBackgroundColor = '#99D4DD';
+export const cardButtonBackgroundColor = '#FDCDBA';
