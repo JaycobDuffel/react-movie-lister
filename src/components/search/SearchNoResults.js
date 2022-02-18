6c725af03ad81bc9cdc1a91b195ef1237b878d18
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:47:41 -0800

    Create and style SearchNoResults.js

--- src/components/search/SearchNoResults.js
+++ src/components/search/SearchNoResults.js
@@ -2,3 +2,36 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { cardBackgroundColor } from '../../constants/colors';
 
+export default function SearchNoResults() {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.text, styles.title]}>Oops!</Text>
+      <Text style={styles.text}>Looks like we didn't find any results for your search.</Text>
+      <Text style={styles.text}>Click below to clear and try again!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: '600',
+  },
+  button: {
+    marginVertical: 15,
+    width: '60%'
+  },
+  container: {
+    backgroundColor: cardBackgroundColor,
+    borderRadius: 25,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
+  }
+});
