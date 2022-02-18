f8bbff91dca02326bd428597a08bca2b226407a9
Jaycob <jaycobduffel@gmail.com>
2022-02-17 17:45:32 -0800

    Add SearchBar component

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -4,3 +4,20 @@ import Constants from 'expo-constants';
 
 import { FilterContext } from '../../context/Context';
 import { searchBarBackgroundColor, searchBarTextColor } from '../../constants/colors';
+
+export default SearchBar = forwardRef((props, ref) => {
+  const { titleFilter, setTitleFilter } = useContext(FilterContext);
+
+  return (
+    <TextInput
+      className="searchInput"
+      id={titleFilter}
+      onChangeText={text => setTitleFilter(text)}
+      placeholder="Search by title..."
+      placeholderTextColor={searchBarTextColor}
+      style={styles.searchBar}
+      type="text"
+      value={titleFilter}
+    ></TextInput>
+  )
+});
